03e645c3ccc3861097c7831384351ca10f406fd3
Funami580 <63090225+Funami580@users.noreply.github.com>
2023-07-06 08:49:53 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# bs.to-linkgen
+
+## Requirements
+```
+python -m pip install requests
+python -m pip install beautifulsoup4
+```
+
+## Example
+
+```
+$ python genlinks.py 'https://bs.to/serie/Yuru-Yuri/1/des'
+https://bs.to/serie/Yuru-Yuri/1/1-Middle-School-Debut/des/Streamtape
+https://bs.to/serie/Yuru-Yuri/1/2-Me-and-You-and-the-Student-Council/des/Streamtape
+https://bs.to/serie/Yuru-Yuri/1/3-You-Wanna-Come-Visit-Yeah-Let-s-Go/des/VOE
+https://bs.to/serie/Yuru-Yuri/1/4-The-Great-Summer-Harvest/des/VOE
+https://bs.to/serie/Yuru-Yuri/1/5-When-Akari-and-the-Cicadas-Cry/des/Streamtape
+https://bs.to/serie/Yuru-Yuri/1/6-Art-Arter-Artist/des/Streamtape
+https://bs.to/serie/Yuru-Yuri/1/7-Christmasery/des/Streamtape
+https://bs.to/serie/Yuru-Yuri/1/8-April-s-Fool/des/Streamtape
+https://bs.to/serie/Yuru-Yuri/1/9-I-m-Not-Scared-of-This-Summer/des/Streamtape
+https://bs.to/serie/Yuru-Yuri/1/10-It-s-a-School-Trip-But-What-Are-We-Here-to-Learn-I-Wonder/des/Streamtape
+https://bs.to/serie/Yuru-Yuri/1/11-Our-Amusement-Club/des/Streamtape
+https://bs.to/serie/Yuru-Yuri/1/12-Warm-Slumber-Party-With-Everyone/des/Streamtape
+```
+
+You can then copy the links and use [JDownloader 2](https://jdownloader.org/jdownloader2) to download the videos.
---
+++ genlinks.py
@@ -0,0 +1,38 @@
+import sys
+import math
+import requests
+from bs4 import BeautifulSoup
+
+PREFERRED_STREAMS = ["Vidoza", "Streamtape", "VOE"]
+
+def get_links(url: str):
+    all_links = []
+    response = requests.get(url)
+    assert response.status_code == 200
+    parsed = BeautifulSoup(response.content, "html.parser")
+    episodes = parsed.find("table", class_="episodes")
+    for i, episode in enumerate(episodes.find_all("tr"), start=1):
+        links = episode.find_all("td")[-1]
+        sorted_links = []
+        for link in links.find_all("a"):
+            url = link["href"]
+            if url.startswith("serie"):
+                url = "https://bs.to/" + url
+            elif url.startswith("/serie"):
+                url = "https://bs.to" + url
+            title = link["title"]
+            try:
+                sorted_links.append((url, PREFERRED_STREAMS.index(title)))
+            except ValueError:
+                sorted_links.append((url, math.inf))
+        if sorted_links:
+            min_by_index = min(sorted_links, key=lambda x: x[1])[0]
+            all_links.append(min_by_index)
+        else:
+            print(f"No url found for episode {i}", file=sys.stderr)
+    return all_links
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    for link in get_links(url):
+        print(link)
